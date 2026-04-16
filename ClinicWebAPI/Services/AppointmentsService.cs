3c94fd34bf1099a4a2123da270b61fa69f6d7958
Rafał Święcicki <s31225@pjwstk.edu.pl>
2026-04-16 09:39:56 +0200

    feat: add class managing appointments services

---
+++ ClinicWebAPI/Services/AppointmentsService.cs
@@ -0,0 +1,41 @@
+﻿using ClinicWebAPI.DTOs;
+using Microsoft.Data.SqlClient;
+namespace ClinicWebAPI.Services;
+
+public class AppointmentsService : IAppointmentsService
+{
+    private readonly string _connectionString;
+
+    public AppointmentsService(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("Default") ?? "";
+    }
+    public async Task<AppointmentListDto> GetAllAppointmentsAsync()
+    {
+        string query = "SELECT * FROM Appointments";
+        
+        await using var connection = new SqlConnection(_connectionString);
+        await connection.OpenAsync();
+
+        await using var command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = query;
+
+        var appointmentListDto = new AppointmentListDto()
+        {
+            AppointmentDetails = new List<AppointmentDetailsDto>()
+        };
+
+        var reader = await command.ExecuteReaderAsync();
+        while (await reader.ReadAsync())
+        {
+            var dto = new AppointmentDetailsDto
+            {
+                IdAppointment = reader.GetInt32(0),
+                Status = reader.GetString(1)
+            };
+            appointmentListDto.AppointmentDetails.Add(dto);
+        }
+        return appointmentListDto;
+    }
+}
