dbde2e0916674e662790a88eca2324fc430947f7
Rafał Święcicki <s31225@pjwstk.edu.pl>
2026-04-22 23:38:27 +0200

    feat: add other fields like notes, emails, phone numbers, doctor's name, license number or specialization

--- ClinicWebAPI/DTOs/AppointmentDetailsDto.cs
+++ ClinicWebAPI/DTOs/AppointmentDetailsDto.cs
@@ -4,8 +4,15 @@ public class AppointmentDetailsDto
 {
     public int IdAppointment { get; set; }
     public DateTime AppointmentDate { get; set; }
-    public string Status { get; set; } = string.Empty;
-    public string Reason { get; set; } = string.Empty;
-    public string PatientFullName { get; set; } = string.Empty;
-    public string PatientEmail { get; set; } = string.Empty;
+    public string Status { get; set; } = null!;
+    public string Reason { get; set; } = null!;
+    public string? InternalNotes { get; set; }
+    
+    public string PatientFullName { get; set; } = null!;
+    public string PatientEmail { get; set; } = null!;
+    public string PatientPhoneNumber { get; set; } = null!;
+    
+    public string DoctorFullName { get; set; } = null!;
+    public string DoctorLicenseNumber { get; set; } = null!;
+    public string SpecializationName { get; set; } = null!;
 }
