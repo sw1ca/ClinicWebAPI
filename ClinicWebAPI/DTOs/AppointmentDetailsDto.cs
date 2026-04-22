dc45d03f41aa8f02d4c4a67af7867c759306a802
Rafał Święcicki <s31225@pjwstk.edu.pl>
2026-04-22 19:15:44 +0200

    feat: add features to AppointmentDetailsDto

--- ClinicWebAPI/DTOs/AppointmentDetailsDto.cs
+++ ClinicWebAPI/DTOs/AppointmentDetailsDto.cs
@@ -3,5 +3,9 @@
 public class AppointmentDetailsDto
 {
     public int IdAppointment { get; set; }
+    public DateTime AppointmentDate { get; set; }
     public string Status { get; set; } = string.Empty;
+    public string Reason { get; set; } = string.Empty;
+    public string PatientFullName { get; set; } = string.Empty;
+    public string PatientEmail { get; set; } = string.Empty;
 }
